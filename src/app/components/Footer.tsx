055afdc47e5280c3cb6656b1b3de2aafb089cb56
M.Aman <167163491+AmanImran123@users.noreply.github.com>
2025-01-24 10:32:57 +0000

    Add files via upload

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+      <div className='footer'>
+        <p> Made with love <span className="text-2xl">❤️</span></p>
+        {/* Icons Here */}
+           <a href="https://www.linkedin.com/in/m-amman-a2187a323/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i className="bi bi-linkedin"> </i> </a>
+           <a href="https://github.com/AmanImran123"><i className="bi bi-github"></i> </a>
+           <a href="https://www.facebook.com/imran.aman.7330?rdid=awEHOmxUnyBbyqIE&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F18mqpVfP8q%2F#"><i className="bi bi-facebook"></i> </a>
+      </div>
+    );
+  }
+  
+  export default Footer
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,21 @@
+
+const Header = () => {
+    return (
+      <div className="navbar">
+        <div className="nav_tags">
+           <p> <a href="https://github.com/AmanImran123"> Home   </a></p>
+           <p>About</p>
+           <p>Contact</p>
+           <p>Blog</p>
+           <p>Need Help?</p>
+           {/* Icons Here */}
+           <a href="https://www.linkedin.com/in/m-amman-a2187a323/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i className="bi bi-linkedin"> </i> </a>
+           <a href="https://github.com/AmanImran123"><i className="bi bi-github"></i> </a>
+           <a href="https://www.facebook.com/imran.aman.7330?rdid=awEHOmxUnyBbyqIE&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F18mqpVfP8q%2F#"><i className="bi bi-facebook"></i> </a>
+        
+        </div>
+      </div>
+    )
+  }
+  
+  export default Header
